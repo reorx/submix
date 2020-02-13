fe6435871bebac81521e9145697617489d0e1832
Reorx <novoreorx@gmail.com>
2020-02-13 10:35:49 +0800

    add datatables assets, pass cli state to views

--- submix/server.py
+++ submix/server.py
@@ -3,10 +3,16 @@ Start a django server from command line,
 this module is not intended to use for production deployment.
 """
 import os
-from . import views
+from .views import cli_state
+from .parser import ProxyURLList
 
 
-def run(sub_source, sub_content, purls):
+def run(sub_source: str, sub_content: bytes, purls: ProxyURLList):
+    # set views state
+    cli_state.sub_source = sub_source
+    cli_state.sub_content = sub_content
+    cli_state.purls = purls
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'submix.settings')
     argv = ['manage.py', 'runserver']
     try:
--- submix/views.py
+++ submix/views.py
@@ -1,17 +1,26 @@
 from django.http import HttpResponse
 from django.views import View
-from django.template import loader
+from django.shortcuts import render
+
+from submix.parser import ProxyURLList
+
+
+class CliState:
+    sub_source: str
+    sub_content: bytes
+    purls: ProxyURLList
+
+
+cli_state = CliState()
 
 
 class IndexView(View):
     def get(self, request):
-        tpl = loader.get_template('index.html')
-        html = tpl.render()
-        return HttpResponse(html)
+        return render(request, 'index.html')
 
 
 class NodesView(View):
     def get(self, request):
-        tpl = loader.get_template('nodes.html')
-        html = tpl.render()
-        return HttpResponse(html)
+        return render(request, 'nodes.html', dict(
+            cli_state=cli_state,
+        ))
