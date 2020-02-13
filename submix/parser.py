567c5361212b11d575bac14d775a75867ad8fccd
Reorx <novoreorx@gmail.com>
2020-02-13 11:06:46 +0800

    add classmethod Node.new_from_url

--- submix/parser.py
+++ submix/parser.py
@@ -4,31 +4,53 @@ it will be the only supported format in the foreseeable future,
 so this module is the parser for "base64 scheme subscription config"
 """
 
+from typing import List
+from dataclasses import dataclass, field
+
 import json
 import base64
-from typing import List
 from urllib.parse import urlparse, ParseResult
+from .utils import base64_encode_str
 
 
+@dataclass
 class Node:
-    raw: str
-    parsed: ParseResult
-    scheme: str
-    data_str: str
-    data: dict
     name: str
+    protocol: str
+    data: dict
+    _data_str: str = field(init=False, repr=False)
+    _url: str = field(init=False, repr=False)
+    _url_parsed: ParseResult = field(init=False, repr=False)
 
-    def __init__(self, raw):
-        self.raw = raw
-        self.parsed = urlparse(self.raw)
-        self.scheme = self.parsed.scheme
-        self.data_str = base64.b64decode(self.parsed.netloc.encode()).decode()
-        self.data = json.loads(self.data_str)
-        if self.scheme == 'vmess':
-            self.name = self.data['ps']
-        else:
-            self.name = ''
-        #print(self.data)
+    def get_url(self) -> str:
+        if self._url:
+            return self._url
+        netloc = base64_encode_str(json.dumps(self.data))
+        return f'{self.protocol}://{netloc}'
+
+    @classmethod
+    def new_from_url(cls, url):
+        url_parsed = urlparse(url)
+        protocol = url_parsed.scheme
+        data_str = base64.b64decode(url_parsed.netloc.encode()).decode()
+        data = json.loads(data_str)
+        name = ''
+        if protocol == 'vmess':
+            name = data['ps']
+
+        node = cls(
+            protocol=protocol,
+            data=data,
+            name=name,
+        )
+
+        # set private attrs
+        node._url = url
+        node._url_parsed = url_parsed
+        node._data_str = data_str
+
+        # print(node.data)
+        return node
 
 
 NodeList = List[Node]
@@ -37,11 +59,11 @@ NodeList = List[Node]
 def parse_raw_sub(raw: bytes) -> NodeList:
     nodes = []
     decoded = base64.b64decode(raw).decode()
-    #print(decoded)
+    # print(decoded)
     for line in decoded.split('\n'):
         if not line:
             continue
-        node = Node(line)
-        print(f'{node.name}:\n  {node.raw}')
+        node = Node.new_from_url(line)
+        print(f'{node.name}:\n  {node.get_url()}')
         nodes.append(node)
     return nodes
---
+++ submix/utils.py
@@ -0,0 +1,9 @@
+import base64
+
+
+def base64_encode_str(s: str) -> str:
+    return base64.b64encode(s.encode()).decode()
+
+
+def base64_decode_str(s: str) -> str:
+    return base64.b64decode(s.encode()).decode()
